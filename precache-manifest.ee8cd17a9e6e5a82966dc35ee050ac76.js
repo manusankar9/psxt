9d099816a27d0616abb75cab129c238bf1618688
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-01 18:25:42 +0000

    Deploy sivasankar9/psxt to github.com/sivasankar9/psxt.git:gh-pages

---
+++ precache-manifest.ee8cd17a9e6e5a82966dc35ee050ac76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f970fa9c857acbc0a2124a560c64c967",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dc4da9214f3016adb66",
+    "url": "/static/css/main.170f56e8.chunk.css"
+  },
+  {
+    "revision": "08a49f25bc4a649bf945",
+    "url": "/static/js/2.b490271c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b490271c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc4da9214f3016adb66",
+    "url": "/static/js/main.0f7263cf.chunk.js"
+  },
+  {
+    "revision": "2feb495fc46f0810df26",
+    "url": "/static/js/runtime-main.c2ad7f64.js"
+  }
+]);
